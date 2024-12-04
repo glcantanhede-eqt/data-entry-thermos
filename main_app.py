ea6a1c3cc44579b7579cfd7b4a6d6d4d6cf60992
GABRIEL LUCAS CANTANHEDE DA SILVA <gabriel.cantanhede@equatorialenergia.com.br>
2024-12-04 11:08:18 -0300

    Hiding header and footer from users

--- main_app.py
+++ main_app.py
@@ -1,22 +1,23 @@
 import streamlit as st
-# Imagem bg pra usar -> https://energiaemdia.equatorialenergia.com.br/img/bg.webp
+
+
+# Background image to use -> https://energiaemdia.equatorialenergia.com.br/img/bg.webp
 st.logo("https://www.equatorialenergia.com.br/wp-content/themes/equatorial-energia-child/img/logo-blue.png")
 
 #st.set_page_config(page_title="Termômetro Reputacional", page_icon=":thermometer:", layout="wide", initial_sidebar_state="expanded") # makes the widgets expand to the full lenght of the screen almost
-st.set_page_config(page_title="Termômetro Reputacional", page_icon=":thermometer:", layout="centered", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Termômetro Reputacional", page_icon=":thermometer:", layout="centered",) # initial_sidebar_state="collapsed")
 
 ### Disabling the streamlit menu on production
-# styles = {
-#         "nav": {
-#             "background-color": "white",
-#             "display": "flex",
-            
-#             "height": ".01rem"
-#         },
-#     }
-# options = {
-#     'show_menu': False
-# }#####
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            footer:after {
+                content: 'Gerência Corporativa de Comunicação Externa, Marketing e Sustentabilidade'}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 ### Customizing the CSS, old style
 with open('style.css') as f:
